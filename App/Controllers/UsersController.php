df80457a670b90c738128ca33315b58df0f5ceda
razanor <vsokologorsky@gmail.com>
2018-05-09 14:18:28 +0300

    fixed small bugs

--- App/Controllers/UsersController.php
+++ App/Controllers/UsersController.php
@@ -16,7 +16,7 @@ class UsersController
         
        Thanks for signing up!
        Your account has been created, you can login with the following credentials after you have activated your account by pressing the url below.
-        
+       
        ------------------------
        Username: '.$name.'
        ------------------------
