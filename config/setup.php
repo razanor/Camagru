17c9d96593fb6b38156bcd3a79db0fdd286c3f25
Nazarii Repak <nazara.re22@gmail.com>
2018-05-14 14:21:26 +0300

    test

--- config/setup.php
+++ config/setup.php
@@ -3,7 +3,7 @@
 $dir = '../uploads';
 
 if (is_dir($dir)) {
-    $files = glob($dir . '/*'); //get all file names
+    $files = glob($dir . '/*'); //get all file name
         foreach($files as $file) {
             if (is_file($file))
             unlink($file); //delete file
