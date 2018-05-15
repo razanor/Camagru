77c6fc58e192487b69456902ebc583647b920354
Nazarii Repak <nazara.re22@gmail.com>
2018-05-15 18:16:21 +0300

    create add page

--- App/Views/add.php
+++ App/Views/add.php
@@ -11,9 +11,31 @@
 
 <body>
 
+<!-- Top navigation -->
+<div class="topnav">
+	  <!-- Centered header -->
+	<div class="topnav-centered">
+		<a style="pointer-events: none; cursor: default;" href="" id="header" class="active">Camagru</a>
+	</div>
+
+	<!-- Left-aligned links (default) -->
+	<a class="add" href="/user-page/">Back</a>
+
+	 <!-- Right-aligned links -->
+	<div class="topnav-right">
+		<a class="cabinet" href="/edit/">Cabinet</a>
+		<a class="logout" href="/logout/">Log out</a>
+	</div>
+
+</div>
 <form action="" method="post" enctype="multipart/form-data">
 <input type="file" name="file"><br>
 <input type="submit" name="submit" value="Upload">
 </form>
+
+<footer>
+	<p>&#169 nrepak, 2018</p>
+</footer>
+
 </body>
 </html>
