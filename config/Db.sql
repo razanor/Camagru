e92f721491b0a76148acae3659cfc9303510065c
razanor <nazara.re22@gmail.com>
2018-05-29 01:07:05 +0300

    setup works

---
+++ config/Db.sql
@@ -0,0 +1,47 @@
+DROP TABLE IF EXISTS `camagru`.`users`;
+DROP TABLE IF EXISTS `camagru`.`images`;
+DROP TABLE IF EXISTS `camagru`.`comments`;
+DROP TABLE IF EXISTS `camagru`.`likes`;
+DROP DATABASE IF EXISTS `camagru`;
+
+CREATE DATABASE IF NOT EXISTS `camagru`;
+
+CREATE TABLE  IF NOT EXISTS `camagru`.`users` ( 
+  `id`            INT(11)         NOT NULL      AUTO_INCREMENT , 
+  `username`      VARCHAR(55)     NOT NULL , 
+  `email`         VARCHAR(55)     NOT NULL , 
+  `password`      VARCHAR(512)    NOT NULL , 
+  `hash`          VARCHAR(255)    NOT NULL , 
+  `activation`    INT(2)          NOT NULL      DEFAULT '0', 
+  `notification`  INT             NOT NULL      DEFAULT '0', 
+  `joined`        TIMESTAMP       NOT NULL      DEFAULT CURRENT_TIMESTAMP ,
+
+  PRIMARY KEY (`id`)
+  ); 
+
+
+CREATE TABLE  IF NOT EXISTS `camagru`.`images` ( 
+  `id`            INT(11)         NOT NULL      AUTO_INCREMENT , 
+  `path`          VARCHAR(128)    NOT NULL , 
+  `likes`         INT(11)         NOT NULL      DEFAULT '0', 
+  `userId`        INT             NOT NULL , 
+  `creation`      TIMESTAMP       NOT NULL      DEFAULT CURRENT_TIMESTAMP ,
+
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE  IF NOT EXISTS `camagru`.`comments` ( 
+  `id`            INT             NOT NULL      AUTO_INCREMENT , 
+  `username`      VARCHAR(55)     NOT NULL , 
+  `comment`       VARCHAR(512)    NOT NULL , 
+  `userId`        INT(11)         NOT NULL , 
+  `imgId`         INT(11)         NOT NULL ,
+
+  PRIMARY KEY (`id`)
+  ); 
+
+
+CREATE TABLE  IF NOT EXISTS `camagru`.`likes` ( 
+  `userId`        INT(11)         NOT NULL , 
+  `imgId`         INT(11)         NOT NULL 
+  ); 
--- config/setup.php
+++ config/setup.php
@@ -1,5 +1,20 @@
 <?php
 
+require_once('database.php');
+
+$sql = file_get_contents('Db.sql');
+try {
+	$dsn = "mysql:host=" . DB_HOST . ";charset=utf8";
+	$db = new PDO($dsn, DB_USER, DB_PASSWORD);
+} catch (\PDOException $e) {
+	echo $e->getMessage();
+}
+$res = $db->query($sql);
+if (isset($_SESSION['user'])) {
+	unset($_SESSION['user']);
+}
+
+
 $dir = '../uploads';
 
 if (is_dir($dir)) {
@@ -11,3 +26,8 @@ if (is_dir($dir)) {
 	rmdir($dir);
 }
 mkdir($dir);
+
+echo "
+<h4>DB re-created</h4>
+<a href='/'>Go to main page!</a>
+";
